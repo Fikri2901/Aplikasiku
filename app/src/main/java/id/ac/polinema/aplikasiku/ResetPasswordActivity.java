089c806546803a2bd5c697c3fffec1db2c8b88b9
Fikri2901 <fikriismudi@gmail.com>
2020-02-12 10:33:56 +0700

    finishing

--- app/src/main/java/id/ac/polinema/aplikasiku/ResetPasswordActivity.java
+++ app/src/main/java/id/ac/polinema/aplikasiku/ResetPasswordActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class ResetPasswordActivity extends AppCompatActivity {
 
@@ -12,10 +13,20 @@ public class ResetPasswordActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_reset_password);
+
+		Button button = (Button) findViewById(R.id.button_pass);
+
+		button.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(ResetPasswordActivity.this, SuccessResetPasswordActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 
-	public void postChangePassword(View view) {
-		Intent intent = new Intent(ResetPasswordActivity.this, SuccessResetPasswordActivity.class);
-		startActivity(intent);
-	}
+//	public void postChangePassword(View view) {
+//		Intent intent = new Intent(ResetPasswordActivity.this, SuccessResetPasswordActivity.class);
+//		startActivity(intent);
+//	}
 }
